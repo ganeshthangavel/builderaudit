b54f4b925d326ba35da4a7e2cdf53b68a7a85188
ganeshthangavel <gthangavel1@gmail.com>
2026-06-13 01:25:27 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1391,4 +1391,3 @@ app.get('/api/_diag/schema', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => console.log('Server running on port ' + PORT));
-     
